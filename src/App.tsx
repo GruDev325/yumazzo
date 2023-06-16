3f443012fff45db3ec8cbb4efa01d59471270a3d
grudev325 <seniordev1225@gmail.com>
2023-06-16 14:05:18 -0300

    add router, addRecipe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LandingPage from './pages/LandingPage';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import { setAllRecipes } from './slices/recipeSlice';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
-axios.defaults.baseURL = "https://master-7rqtwti-yj2le3kr2yhmu.uk-1.platformsh.site/yumazoo";
+import LandingPage from "./pages/LandingPage";
+import AddRecipe from "./pages/AddRecipe";
+import { setAllRecipes } from "./slices/recipeSlice";
+
+axios.defaults.baseURL =
+  "https://master-7rqtwti-yj2le3kr2yhmu.uk-1.platformsh.site/yumazoo";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,76 +16,112 @@ function App() {
   const allRecipes = useSelector((state: any) => state.recipe.allRecipes);
 
   useEffect(() => {
-    axios.get("/recipes").then(res => {
+    axios.get("/recipes").then((res) => {
       const returnData = res.data.message;
-      dispatch(setAllRecipes(returnData.map((item: any) => {
-        const produces = item.produce.split(",").map((item: string) => item.trim());
-        return {
-          ...item,
-          ingredient: [{
-            name: "Protein",
-            values: [{
-              text: item.protein,
-              className: 'lower'
-            }]
-          }, {
-            name: "Produce",
-            values: [{
-              text: produces[0],
-              className: 'foliage'
-            }, {
-              text: produces[1],
-              className: 'red'
-            }]
-          }, {
-            name: "Spices",
-            values: [{
-              text: item.spice,
-              className: 'sunfire'
-            }]
-          }, {
-            name: "Cooking Oil",
-            values: [{
-              text: item.cookingOil,
-              className: 'sunfire'
-            }]
-          }, {
-            name: "Volume/Weight",
-            values: [{
-              text: item.volume,
-              className: 'lower'
-            }]
-          }, {
-            name: "Serves",
-            values: [{
-              text: item.serves,
-              className: 'lower'
-            }]
-          }, {
-            name: "Authenticity",
-            values: [{
-              text: item.authenticity,
-              className: 'sunfire'
-            }]
-          }, {
-            name: "Stock",
-            values: [{
-              text: item.stock,
-              className: 'sunfire'
-            }]
-          }]
-        };
-      })));
-    })
+      dispatch(
+        setAllRecipes(
+          returnData.map((item: any) => {
+            const produces = item.produce
+              .split(",")
+              .map((item: string) => item.trim());
+            return {
+              ...item,
+              ingredient: [
+                {
+                  name: "Protein",
+                  values: [
+                    {
+                      text: item.protein,
+                      className: "lower",
+                    },
+                  ],
+                },
+                {
+                  name: "Produce",
+                  values: [
+                    {
+                      text: produces[0],
+                      className: "foliage",
+                    },
+                    {
+                      text: produces[1],
+                      className: "red",
+                    },
+                  ],
+                },
+                {
+                  name: "Spices",
+                  values: [
+                    {
+                      text: item.spice,
+                      className: "sunfire",
+                    },
+                  ],
+                },
+                {
+                  name: "Cooking Oil",
+                  values: [
+                    {
+                      text: item.cookingOil,
+                      className: "sunfire",
+                    },
+                  ],
+                },
+                {
+                  name: "Volume/Weight",
+                  values: [
+                    {
+                      text: item.volume,
+                      className: "lower",
+                    },
+                  ],
+                },
+                {
+                  name: "Serves",
+                  values: [
+                    {
+                      text: item.serves,
+                      className: "lower",
+                    },
+                  ],
+                },
+                {
+                  name: "Authenticity",
+                  values: [
+                    {
+                      text: item.authenticity,
+                      className: "sunfire",
+                    },
+                  ],
+                },
+                {
+                  name: "Stock",
+                  values: [
+                    {
+                      text: item.stock,
+                      className: "sunfire",
+                    },
+                  ],
+                },
+              ],
+            };
+          })
+        )
+      );
+    });
   }, []);
 
   useEffect(() => {
     console.log(allRecipes);
   }, [allRecipes]);
   return allRecipes.length > 0 ? (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/add" element={<AddRecipe />}></Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   ) : null;
 }
 
---
+++ src/pages/AddRecipe/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const style: React.CSSProperties = {
+  backgroundColor: "red",
+};
+const AddRecipe = () => {
+  return <div style={style}>AddRecipe</div>;
+};
+
+export default AddRecipe;
